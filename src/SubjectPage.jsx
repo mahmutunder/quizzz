f2b0a7addb0a94471b3cc8216afc8c91035fe1aa
mahmutunder <mahmutunder@gmail.com>
2025-02-14 21:01:22 -0700

    Fixed routing issue on GitHub Pages

--- src/SubjectPage.jsx
+++ src/SubjectPage.jsx
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import htmlData from "public/data/html.json";
+import htmlData from "..html.json";
 
 const sub= {
 html:htmlData
